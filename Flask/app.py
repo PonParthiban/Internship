cc9225e2e1ce3d5e3f23cea60b8796a9c4b0580a
PonParthiban <parthi2006a@gmail.com>
2026-04-22 16:16:42 +0530

    flask basic

--- Flask/app.py
+++ Flask/app.py
@@ -1,23 +1,17 @@
-from flask import Flask, request, render_template,url_for
-from flask_sqlalchemy import SQLAlchemy
-
-
+from flask import Flask, request, render_template,url_for,redirect
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' #//// -> absolute path , /// -> relative path
-db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.String(200),nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default = datetime.utcnow )
-     
-    def __repr__(self):
-        return '<Task %r>' % self.id
+@app.route("/")
+def home():
+    return "hello this is home page \n <h2>hello</h2>"
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run(debug=True)
