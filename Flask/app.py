74f3b68f939b9da15225fddec4a1863e598fd459
PonParthiban <parthi2006a@gmail.com>
2026-04-24 10:31:41 +0530

    message flash

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template,url_for,redirect,session
+from flask import Flask, request, render_template,url_for,redirect,session,flash
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -7,33 +7,37 @@ app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/")
 def home():
-    return "hello this is home page \n <h2>hello</h2>"
+    return render_template("index.html")
 
 @app.route("/login", methods = ["POST", "GET"])
 def login():
     if request.method == "POST":
-      session.permanent = True
+      session.permanent = True 
       user =  request.form["nm"]
       session["user"] = user
+      flash("login sucessful!")
       return redirect(url_for("user"))
     else:
       if "user" in session:
+         flash("Already logged in!")
          return redirect(url_for("user"))
       return render_template("login.html")
 @app.route("/user")
 def user():
     if "user" in session:
        user = session["user"]
-       return f"<h1>{user}</h1>"
+       return render_template("user.html", user=user)
     else:
        if "user" in session:
           user = session["user"]
           return f"<h1>{user}<h1/>"
        else:
+          flash("you are not logged in")
           return redirect (url_for("login"))
     
 @app.route("/logout")
 def logout():
+   flash(f"You have been logged out!", "info")
    session.pop("user", None)
    return redirect (url_for("login"))
 
