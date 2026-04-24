2ebe16c92e0b41f84095c492a0c344d9dc646bea
PonParthiban <parthi2006a@gmail.com>
2026-04-24 16:02:01 +0530

    sql added

--- Flask/flask_api/main.py
+++ Flask/flask_api/main.py
@@ -1,22 +1,71 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse, fields, marshal_with, abort
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask("__name__")
+app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-names = {"parthi": {"age": 19, "gender":"male"},
-         "jim": {"age": 32, "gender":"male"}}
+class VideoModel(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String(100), nullable=False)
+     views = db.Column(db.Integer, nullable=False)
+     likes = db.Column(db.Integer, nullable=False)
+     
+     def __repr__(self):
+        return f"Video(name={self.name}, views={self.views}, likes={self.likes})"    
+     
 
+vid_put_args = reqparse.RequestParser()
+vid_put_args.add_argument("name", type=str, help="name of the video",required=True)
+vid_put_args.add_argument("views", type=int, help="views of the video",required=True)
+vid_put_args.add_argument("likes", type=int, help="likes of the video",required=True)
 
-class helloworld(Resource):
-      def get(self,name):
-          return names[name]
-      
-      def post(self):
-           return {"data" : "data posted"}
+resource_fields = {
+    "id": fields.Integer,
+    "name": fields.String,
+    "views": fields.Integer,
+    "likes": fields.Integer
+}
 
-api.add_resource(helloworld, "/helloworld/<string:name>")
+class Video(Resource):
+
+    @marshal_with(resource_fields)
+    def get(self, vid_id):
+        video = VideoModel.query.filter_by(id=vid_id).first()
+
+        if not video:
+            abort(404, message="Video not found")
+        return video
+    
+    @marshal_with(resource_fields)
+    def put(self, vid_id):
+        args = vid_put_args.parse_args()
+        video = VideoModel(id=vid_id, name=args["name"], views=args["views"], likes=args["likes"])
+
+        if VideoModel.query.get(vid_id):
+            abort(409, message="Video already exists")
+
+        db.session.add(video)
+        db.session.commit()
+
+        return video, 201   
+    def delete(self, vid_id):
+        video = VideoModel.query.get(vid_id)
+
+        if not video:
+            abort(404, message="Video not found")
+
+        db.session.delete(video)
+        db.session.commit()
+
+        return {"message": "Deleted successfully"}
+                
+api.add_resource(Video, "/video/<int:vid_id>")
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
--- Flask/flask_api/test.py
+++ Flask/flask_api/test.py
@@ -2,5 +2,20 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld/parthi")
-print(response.json())
+data = [{"likes": 10, "name": "Flask Tutorial","views": 100},
+        {"likes": 20, "name": "Django Tutorial","views": 200},
+        {"likes": 30, "name": "FastAPI Tutorial","views": 300}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), json=  data[i])
+    print("PUT:", response.json())
+
+input()
+
+response = requests.delete(BASE + "video/1")
+print("DELETE:", response.status_code)
+
+input()
+
+response = requests.get(BASE + "video/6")
+print("GET:", response.json())
