c8f813723120dd0327578b1ae803b30f1f8a8818
Pon Parthiban <parthi2006a@gmail.com>
2026-05-12 16:46:55 +0530

    more ml concepts

--- 20.ML/Supervised Learning/Naive_Bayes/main.py
+++ 20.ML/Supervised Learning/Naive_Bayes/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from string import punctuation
 
@@ -16,15 +16,12 @@ print(df['label'].value_counts())
 def data_clean(text):
     text = text.lower()
     result = ""
-
     # Remove punctuation
     for char in text:
         if char not in punctuation:
             result += char
-
     # Remove extra whitespaces
     result = " ".join(result.split())
-
     return result
 
 df['label'] = df['label'].map({
@@ -41,12 +38,12 @@ print(df.head())
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=42, stratify=y)
 
-vectorizer = TfidfVectorizer()
+vectorizer = TfidfVectorizer(stop_words='english')
 
 X_train = vectorizer.fit_transform(X_train)
 X_test = vectorizer.transform(X_test)
 
-model = LogisticRegression()
+model = MultinomialNB()
 model.fit(X_train, y_train)
 
 prediction = model.predict(X_test)
@@ -58,4 +55,3 @@ confuse = confusion_matrix(y_test, prediction)
 print(f"Accuracy: {accuracy}")
 print(f"classification report: {classif}")
 print(f"confuse matrix: {confuse}")
-
